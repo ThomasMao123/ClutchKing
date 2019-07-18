031791fa59276b40f86e134396da7da08abc0c4c
ThomasMao123 <haoyum2@illinois.edu>
2019-07-17 23:49:24 -0500

    Finish advanced SQL queries.

--- Web/search_champion_stats.php
+++ Web/search_champion_stats.php
@@ -8,12 +8,21 @@
     
     $connector = new mysqli ($host, $dbusername, $dbpassword, $dbname);
     
-    $result = mysqli_query($connector, "SELECT * FROM single_champion_stats WHERE Name = '$champion_name' AND Role = 'top';");
     
-    $data = array();
+    
+    $best_counter_search = "SELECT * FROM single_champion_stats WHERE Name IN (SELECT Name FROM match_up_stats WHERE ".$champion_name." IN (SELECT MIN(TEMP.A) FROM (SELECT Name AS N, ".$champion_name." AS A FROM match_up_stats WHERE NOT Name = ".$champion_name.") AS TEMP))";
+    $best_against_search = "SELECT * FROM single_champion_stats WHERE Name IN (SELECT Name FROM match_up_stats WHERE ".$champion_name." IN (SELECT MAX(TEMP.A) FROM (SELECT Name AS N, ".$champion_name." AS A FROM match_up_stats WHERE NOT Name = ".$champion_name.") AS TEMP))";
 
+    $data = array();
+    $result = mysqli_query($connector, "SELECT * FROM single_champion_stats WHERE Name = '$champion_name' AND Role = 'top';");
     while($row = mysqli_fetch_assoc($result)) {
         $data[] = $row;
     }
 
+    $best_counter_result = mysqli_query($connector, $best_counter_search);
+    $data[] = mysqli_fetch_assoc($best_counter_result);
+
+    $best_against_result = mysqli_query($connector, $best_against_search);
+    $data[] = mysqli_fetch_assoc($best_against_result);
+    
     echo json_encode($data);
